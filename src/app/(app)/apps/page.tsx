afd6aa5e62af7dd6a472cfeafb47073dc01eb7e5
ClearQ Developer <dev@clearq.net>
2026-04-17 15:39:06 -0400

    fix: move dynamic export to top of file

--- src/app/(app)/apps/page.tsx
+++ src/app/(app)/apps/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
@@ -5,8 +7,6 @@ const AppsClient = dynamic(() => import("./AppsClient"), {
   ssr: false,
   loading: () => <div className="flex h-screen items-center justify-center bg-zinc-950 text-zinc-500">Loading App Store...</div>
 });
-
-export const dynamic = "force-dynamic";
 
 export default function AppsPage() {
   return (
